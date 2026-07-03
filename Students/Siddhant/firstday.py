b8d45c6859518a2a4f8f7e8a8dc8eaef8e4ee505
Siddhant Trivedi <sd.trivedi2014@gmail.com>
2026-07-03 21:43:07 +0530

    Added Day1 Task

---
+++ Students/Siddhant/firstday.py
@@ -0,0 +1,15 @@
+# 03/07/2026
+
+print("Hello, I am Siddhant")
+
+a = 10
+b = 20
+
+print("Addition:", a + b)
+print("Subtraction:", b - a)
+print("Multiplication:", a * b)
+
+for i in range(1, 6):
+    print("Number:", i)
+
+print("First Task Completed..")
