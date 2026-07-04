e928fb106c299de7ba29883242c3b351a92088e6
Yuvraj Vinodkumar Verma <verma05112005@gmail.com>
2026-07-04 19:42:08 +0530

    Added Yuvraj Day 1 and Day 2

---
+++ Students/Yuvraj/day1.py
@@ -0,0 +1,13 @@
+# Q1 to check my string in string enterd by user
+x=input("Enter ths string: ")
+print("If my is in",x,'my' in x)
+
+# Q2 to check wheter two string entered by user refer to same object or not
+print("Enter the two strings:")
+s1=input()
+s2=input()
+print(s1,id(s1),s2,id(s2),s1 is s2)
+
+# Q3 result of 5>10<5
+print(5>10<5)
+
