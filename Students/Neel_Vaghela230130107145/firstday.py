9b2f47c422b31ace74db8c09e90ef3912976a747
NeelVaghela-230130107145 <230130107145@gecg.ac.in>
2026-07-04 14:10:56 +0530

    Added Python internship task

---
+++ Students/Neel_Vaghela230130107145/firstday.py
@@ -0,0 +1,20 @@
+# 03/07/2026
+
+print("Hello, I am Neel Vaghela")
+
+name = "Python"
+marks = 85
+
+print("Subject:", name)
+print("Marks:", marks)
+
+if marks >= 35:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
+
+print("\nNumbers from 1 to 5:")
+for i in range(1, 6):
+    print(i)
+
+print("\nTask Completed Successfully!")
