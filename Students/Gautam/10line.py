fc740cf43801fb231efc4ad060d3c68dbbaac9f2
Gautamparmar2006 <230130107074@gecg28.ac.in>
2026-07-04 14:20:51 +0530

    day1 task

---
+++ Students/Gautam/10line.py
@@ -0,0 +1,10 @@
+print("Hello i am Gautam")
+print("My code is for multiplication table.")
+num = int(input("Enter a number: "))
+print("Multiplication Table:")
+for i in range(1, 11):
+    result = num * i
+    print(f"{num} x {i} = {result}")
+print("Done!")
+print("Thank you,")
+print("Program ended.")
