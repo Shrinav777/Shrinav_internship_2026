ad6b4663e30a2207a9753deaa2f4a3242f5dbad4
harshilboricha <103097194+harshilboricha@users.noreply.github.com>
2026-07-06 19:48:47 +0530

    Added Spring Boot CRUD project

---
+++ Students/Harshil_240133107003/copper-rivet-management/src/main/java/com/example/copper_rivet_management/CopperRivetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.copper_rivet_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopperRivetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopperRivetManagementApplication.class, args);
+	}
+
+}
