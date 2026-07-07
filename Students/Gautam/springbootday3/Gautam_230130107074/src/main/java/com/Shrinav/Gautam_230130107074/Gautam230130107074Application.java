075133c6697c84fc52e082a5054c3e5f4ae43269
Gautamparamr2006 <23013107074@gecg28.ac.in>
2026-07-07 20:56:08 +0530

    day3 task update

---
+++ Students/Gautam/springbootday3/Gautam_230130107074/src/main/java/com/Shrinav/Gautam_230130107074/Gautam230130107074Application.java
@@ -0,0 +1,13 @@
+package com.Shrinav.Gautam_230130107074;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gautam230130107074Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gautam230130107074Application.class, args);
+	}
+
+}
