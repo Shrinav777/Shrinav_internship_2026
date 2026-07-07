4231729cf1dc042c3478d90692c09f0eeff5a224
Menyy1126 <meniborad07@gmail.com>
2026-07-07 15:28:43 +0530

    Add tack

---
+++ Students/Mensi_Borad/Day3_Shrinav/src/main/java/com/MensiET23BTCO003/Day3_Shrinav/Day3ShrinavApplication.java
@@ -0,0 +1,13 @@
+package com.MensiET23BTCO003.Day3_Shrinav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ShrinavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3ShrinavApplication.class, args);
+	}
+
+}
