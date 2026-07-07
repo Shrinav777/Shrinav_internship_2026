c365f45b629574dd0ad1f1f2c1b5d125f57f77d9
PavanChaudhari0219 <pavanchaaudhari33@gmail.com>
2026-07-07 14:56:22 +0530

    Create Pavan220130107018Application.java

---
+++ Students/Pavann/day3/PAVAN_220130107018/src/main/java/com/shrinav/PAVAN_220130107018/Pavan220130107018Application.java
@@ -0,0 +1,13 @@
+package com.shrinav.PAVAN_220130107018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pavan220130107018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pavan220130107018Application.class, args);
+	}
+
+}
