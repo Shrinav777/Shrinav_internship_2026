5238dc79092a616291fd0c444e06a557621d0e13
Amarsinh Gohil <amarsinhgohil2004@gmail.com>
2026-07-11 08:21:17 +0530

    react todos task

---
+++ Students/Amarsinh Gohil/react/ludo-game/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Todos from "./Todos";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Todos />
+    </>
+  );
+}
+
+export default App;
